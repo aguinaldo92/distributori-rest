1bad4d10dd584a51a84cf0b17fffb6e5cbb61283
Salvatore <sato89@hotmail.it>
2017-05-05 01:01:38 +0200

    Sistemazione gitignore e PersonaDaoTeest

--- src/test/java/it/unisalento/rest/distributori/daoimpl/PersonaDaoImplTest.java
+++ src/test/java/it/unisalento/rest/distributori/daoimpl/PersonaDaoImplTest.java
@@ -48,6 +48,8 @@ public class PersonaDaoImplTest {
 		
 		boolean mailexists = dao.emailExists("sato89@hotmail.it");
 		assertTrue(mailexists);
+		mailexists = dao.emailExists("test@notexist.junit");
+		assertFalse(mailexists);
 	}
 	@Test
 	public void testCRUD() throws Exception {
