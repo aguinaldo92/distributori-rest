877b8f040516a5b24c2c09ed4a979c26bbd789ed
Salvatore <sato89@hotmail.it>
2017-05-16 14:49:42 +0200

    Aggiunti test per Utils

---
+++ src/test/java/it/unisalento/rest/distributori/util/AllUtilTests.java
@@ -0,0 +1,11 @@
+package it.unisalento.rest.distributori.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ PasswordUtilsTest.class, TokenUtilsTest.class })
+public class AllUtilTests {
+
+}
