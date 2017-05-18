637c2b1f73c6f8298d99afd9d3074c432e022843
Andrea Licastro <aguinaldo.licastro@live.it>
2017-05-18 02:08:02 +0200

    Aggiunti i test a util dal commit numero: 877b8f040516a5b24c2c09ed4a979c26bbd789ed  di master

---
+++ src/test/java/it/unisalento/rest/distributori/util/PasswordUtilsTest.java
@@ -0,0 +1,16 @@
+package it.unisalento.rest.distributori.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PasswordUtilsTest {
+
+	@Test
+	public void testGetSha256() throws Exception {
+		String pw256 = PasswordUtils.getSha256("password-prova");
+		System.out.println(pw256);
+		assertTrue(pw256.equals("ae1fb78a8928bb7e212e5fdb87362e8ec45b0052eec5349e8cf6ba55b3cd0b8e"));
+	}
+
+}
